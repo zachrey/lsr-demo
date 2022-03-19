9813c61efd40174c09a73c5d11820ca446714a85
zachrey <zz__0123@163.com>
2022-03-20 00:43:57 +0800

    feat(all): 增加 andt/charts

--- pages/components/Analysis/Charts.tsx
+++ pages/components/Analysis/Charts.tsx
@@ -1,8 +1,45 @@
 import React from 'react';
+import { Line } from '@ant-design/plots';
+
+const LineCharts: React.FC<{ data: any }> = ({ }) => {
+  const data = [
+    { year: '1991', value: 3 },
+    { year: '1992', value: 4 },
+    { year: '1993', value: 3.5 },
+    { year: '1994', value: 5 },
+    { year: '1995', value: 4.9 },
+    { year: '1996', value: 6 },
+    { year: '1997', value: 7 },
+    { year: '1998', value: 9 },
+    { year: '1999', value: 13 },
+  ];
+
+  const config = {
+    data,
+    title: {
+      visible: true,
+      text: '带数据点的折线图',
+    },
+    xField: 'year',
+    yField: 'value',
+    point: {
+      visible: true,
+      size: 5,
+      shape: 'diamond',
+      style: {
+        fill: 'white',
+        stroke: '#2593fc',
+        lineWidth: 2,
+      },
+    },
+  };
+  return <Line {...config} />;
+};
 
 export default function Charts ({ data }) {
   return (
     <>
+      <LineCharts data={data} />
     </>
   );
 }
